26a09df38053effc9fca7c9f07c9e92232695044
Melvins Simon <melvinssimon@gmail.com>
2024-07-14 17:59:54 +0300

    Add about page

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 const Header = document.querySelector("#Header");
+
 Header.innerHTML = `
-<nav class="w-full pt-1">
+<nav class="w-full py-3">
     <div class="container mx-auto max-sm:px-3 h-14 flex justify-between items-center">
 
         <div class="flex items-center lg:gap-10 gap-8">
@@ -26,16 +27,26 @@ Header.innerHTML = `
 
     </div>
 </nav>
-<div class="h-10 w-full flex lg:gap-10 gap-8 justify-center items-center">
-    <span class="lg:text-xl text-lg font-bold bg-gradient-to-r from-amber-600 cursor-pointer hover:opacity-80 active:opacity-100 ease-in-out duration-200 select-none via-amber-400 to-amber-50 w-max text-transparent bg-clip-text">Trending</span>
-    <span class="lg:text-xl text-lg font-bold bg-gradient-to-r from-amber-600 cursor-pointer hover:opacity-80 active:opacity-100 ease-in-out duration-200 select-none via-amber-400 to-amber-50 w-max text-transparent bg-clip-text">Top Rated</span>
-</div>
+${
+    window.location.href === 'http://127.0.0.1:5500/src/index.html' ?
+    (
+    `<div class="h-10 w-full flex lg:gap-10 gap-8 justify-center items-center">
+        <span class="lg:text-xl text-lg font-bold bg-gradient-to-r from-amber-600 cursor-pointer hover:opacity-80 active:opacity-100 ease-in-out duration-200 select-none via-amber-400 to-amber-50 w-max text-transparent bg-clip-text">Trending</span>
+        <span class="lg:text-xl text-lg font-bold bg-gradient-to-r from-amber-600 cursor-pointer hover:opacity-80 active:opacity-100 ease-in-out duration-200 select-none via-amber-400 to-amber-50 w-max text-transparent bg-clip-text">Top Rated</span>
+    </div>
 
-<div class="pb-2 w-full flex justify-center items-center">
-     <div class="relative w-[80%] lg:w-[30%] mx-auto">
-     <input type="search" placeholder="Search trending movies..." class="w-full pl-8 pr-16 lg:py-3 py-2 rounded-full text-nowrap text-ellipsis outline-none border border-amber-500 lg:text-lg">
-     <i class="fa-solid fa-magnifying-glass absolute top-0 bottom-0 right-8 my-auto z-10 lg:text-2xl text-lg font-bold hover:text-amber-400 h-max cursor-pointer"></i>   
-     </div>
-     
-</div>
+
+    <div class="pb-2 w-full flex justify-center items-center">
+        <div class="relative w-[80%] lg:w-[30%] mx-auto">
+        <input type="search" placeholder="Search trending movies..." class="w-full pl-8 pr-16 lg:py-3 py-2 rounded-full text-nowrap text-ellipsis outline-none border border-amber-500 lg:text-lg">
+        <i class="fa-solid fa-magnifying-glass absolute top-0 bottom-0 right-8 my-auto z-10 lg:text-2xl text-lg font-bold hover:text-amber-400 h-max cursor-pointer"></i>   
+        </div>     
+    </div>
+    `
+    ) : (
+        ''
+    )
+}
 `;
+
+console.log(window.location.href);
